89fcf64413a6f91c795954243b55961db41faac3
Aleksandar Konatar <aleksandar.k03@gmail.com>
2026-02-14 23:05:52 +0100

    select theme

---
+++ src/Aco228.v2.MongoDb/Aco228.MongoDb/Extensions/MongoFiltersExtensions/MongoFiltersStringExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq.Expressions;
+using Aco228.MongoDb.Extensions.FilterDefinitionExtensions;
+using Aco228.MongoDb.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Aco228.MongoDb.Extensions.MongoFiltersExtensions;
+
+public static class MongoFiltersStringExtensions
+{
+    public static LoadSpecification<TDocument, TProjection> RegexString<TDocument, TProjection, TKey>(
+        this LoadSpecification<TDocument, TProjection> spec, 
+        Expression<Func<TDocument, TKey>> selector,
+        string? regex)
+        where TDocument : MongoDocument
+        where TProjection : class
+    {
+        if(string.IsNullOrEmpty(regex)) 
+            return spec;
+        
+        var field = new ExpressionFieldDefinition<TDocument>(selector);
+        var filter = Builders<TDocument>.Filter.Regex(field, new BsonRegularExpression(regex, "i"));
+        
+        spec.FilterDefinitions.Add(filter);
+        return spec;
+    }
+}
