a3579c59a771c95897b4b76cb7f81a08f73c0025
Aleksandar Konatar <aleksandar.k03@gmail.com>
2026-02-20 23:05:21 +0100

    create image style spec ai

--- src/Aco228.v2.MongoDb/Aco228.MongoDb/Models/IdDocument.cs
+++ src/Aco228.v2.MongoDb/Aco228.MongoDb/Models/IdDocument.cs
@@ -8,12 +8,14 @@ public class IdDocument
     [MongoIndex] public string SlugId { get; set; }
     [MongoIndex] public ObjectId Id { get; set; }
     [MongoIndex] public string Name { get; set; } = "";
+    public string? Description { get; set; } = "";
 
     public static IdDocument CreateFrom<T>(T document) where T : SlugDocument
         => new()
         {
             SlugId = document.SlugId,
             Id = document.Id,
-            Name = document.Name
+            Name = document.Name,
+            Description = document.Description,
         };
 }
