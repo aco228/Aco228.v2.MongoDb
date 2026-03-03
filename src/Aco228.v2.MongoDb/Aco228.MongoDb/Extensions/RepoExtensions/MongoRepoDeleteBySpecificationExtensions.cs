cce5baad22a238b76c3bd6d23ac9e9396dc4f70c
Aleksandar Konatar <aleksandar.k03@gmail.com>
2026-03-03 19:09:27 +0100

    delete offers

---
+++ src/Aco228.v2.MongoDb/Aco228.MongoDb/Extensions/RepoExtensions/MongoRepoDeleteBySpecificationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+using Aco228.MongoDb.Models;
+
+namespace Aco228.MongoDb.Extensions.RepoExtensions;
+
+public static class MongoRepoDeleteBySpecificationExtensions
+{
+    public static async Task DeleteAsync<TDocument, TProjection>(
+        this LoadSpecification<TDocument, TProjection> spec,
+        Expression<Func<TDocument, bool>>? filter = null)
+        where TDocument : MongoDocument
+        where TProjection : class
+    {
+        spec.FilterBy(filter);
+        var filters = spec.BuildFilter();
+        await spec.Repo.GetCollection().DeleteManyAsync(filters);
+    }
+}
