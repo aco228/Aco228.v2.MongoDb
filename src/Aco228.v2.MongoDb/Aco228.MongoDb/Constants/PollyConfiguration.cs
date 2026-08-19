54619fd90c842908754c79f174653181d03c8a34
aco228 <aleksandar.k03@gmail.com>
2026-08-19 13:36:41 +0200

    GenerateArticleTitleTemplatesTask multi language approach 2

--- src/Aco228.v2.MongoDb/Aco228.MongoDb/Constants/PollyConfiguration.cs
+++ src/Aco228.v2.MongoDb/Aco228.MongoDb/Constants/PollyConfiguration.cs
@@ -9,16 +9,14 @@ internal static class PollyConfiguration
         .Handle<MongoConnectionException>()
         .Or<MongoNodeIsRecoveringException>()
         .Or<MongoNotPrimaryException>()
-        .OrInner<IOException>() // This catches the "I/O error occurred"
-        .WaitAndRetryAsync(3, retryAttempt => 
-                TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)) // Exponential backoff: 2s, 4s, 8s
-        );
-    
+        .Or<ObjectDisposedException>()
+        .OrInner<IOException>()
+        .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+
     public static readonly ISyncPolicy Sync = Policy
         .Handle<MongoConnectionException>()
         .Or<MongoNodeIsRecoveringException>()
+        .Or<ObjectDisposedException>()
         .OrInner<IOException>()
-        .WaitAndRetry(3, retryAttempt => 
-            TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))
-        );
+        .WaitAndRetry(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
 }
